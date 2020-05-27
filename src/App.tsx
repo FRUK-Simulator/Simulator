c727e0d7586eb429a0f2c142c995e97d44ea80f9
Gino Notto <ginojnotto@gmail.com>
2020-05-26 21:17:34 -0400

    add react redux * add store to react context tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import { store } from './store';
+import { Blockly } from './BlocklyInterface/Blockly';
+import { RobotSimulator } from './RobotSimulator/RobotSimulator';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App" />
+    <Provider store={store}>
+      <div className="App" />
+    </Provider>
+
   );
 }
 
