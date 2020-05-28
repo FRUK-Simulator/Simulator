e581879449eb9314ad98537f70aaf7d8da728064
Gino Notto <ginojnotto@gmail.com>
2020-05-28 09:03:18 -0400

    Remove unused var

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Blockly } from "./BlocklyInterface/Blockly";
 import { RobotSimulator } from "./RobotSimulator/RobotSimulator";
 import { NotFoundView } from "./ErrorViews/NotFoundView";
