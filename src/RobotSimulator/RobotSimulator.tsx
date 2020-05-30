e5236f4cccb6ca3c7636e52f8348aa15c5a59283
JamesWP <JamesWP@users.noreply.github.com>
2020-05-30 17:10:42 +0100

    add basic layout (#13)
    Co-authored-by: James Peach <jpeach6@bloomberg.net>
    Co-authored-by: Gino Notto <ginojnotto@gmail.com>

--- src/RobotSimulator/RobotSimulator.tsx
+++ src/RobotSimulator/RobotSimulator.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+import "./RobotSimulator.css";
 
 export const RobotSimulator: FunctionComponent = () => {
   return <div className="robot-simulator">Robot Simulator</div>;
