cf7eeba00f08fd79f21c0d035afed4574efaf8e7
Gino Notto <ginojnotto@gmail.com>
2020-06-04 21:35:58 -0400

    add blockly actions to reducer

--- src/BlocklyInterface/blocklyReducer.ts
+++ src/BlocklyInterface/blocklyReducer.ts
@@ -1,10 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 /**
  * Reducers for handling the state of the blockly interface such as which blocks are highlighted.
  */
 export const blocklyReducer = createSlice({
-  initialState: {},
+  initialState: {
+    /** The id of the block to highlight */
+    highlightedBlock: "" as string | null,
+    /** The current generated code */
+    code: null as string | null,
+  },
   name: "blockly",
-  reducers: {},
+  reducers: {
+    highlightBlock(state, action: PayloadAction<{ blockId: string }>) {
+      state.highlightedBlock = action.payload.blockId;
+      return state;
+    },
+    setCode(state, action: PayloadAction<{ code: string }>) {
+      state.code = action.payload.code;
+      return state;
+    },
+  },
 });
