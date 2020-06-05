78a98220496946aa926401f25b3b3676f6f4e4f1
Gino Notto <ginojnotto@gmail.com>
2020-06-05 01:02:09 -0400

    add component to connect blockly and vm together

---
+++ src/JavascriptVM/JavascriptVM.tsx
@@ -0,0 +1,75 @@
+import { FunctionComponent, useState } from "react";
+import React from "react";
+
+import "./JavascriptVM.css";
+import { useSelector, useDispatch } from "react-redux";
+import { isExecuting, vmSlice } from "./vmSlice";
+import { getCode } from "../BlocklyInterface/blocklySlice";
+import { AppDispatch } from "../store";
+import { getIntepreter } from "./vm";
+import Interpreter from "js-interpreter";
+
+export const JavascriptVM: FunctionComponent = () => {
+  const [interpreter, setInterpreter] = useState<Interpreter | null>(null);
+  const executing = useSelector(isExecuting);
+  const dispatch = useDispatch<AppDispatch>();
+  const code = useSelector(getCode);
+
+  const startButton = (
+    <button
+      disabled={!code}
+      onClick={() => {
+        if (!code) {
+          return;
+        }
+        dispatch(vmSlice.actions.startExecution());
+        setInterpreter(getIntepreter(code, dispatch));
+      }}
+    >
+      Start
+    </button>
+  );
+
+  const stopButton = (
+    <button
+      onClick={() => {
+        dispatch(vmSlice.actions.stopExecution());
+        setInterpreter(null);
+      }}
+    >
+      Stop
+    </button>
+  );
+
+  const stepButton = (
+    <button
+      onClick={() => {
+        interpreter?.step();
+      }}
+    >
+      Step
+    </button>
+  );
+
+  const runButton = (
+    <button
+      onClick={() => {
+        interpreter?.run();
+      }}
+    >
+      Run
+    </button>
+  );
+
+  const runButtons = [runButton, stepButton];
+
+  return (
+    <div className="javascript-vm">
+      <div>
+        {executing ? runButtons : startButton}
+        {executing && stopButton}
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
--- src/Views/SimulatorView.tsx
+++ src/Views/SimulatorView.tsx
@@ -3,11 +3,13 @@ import { Blockly } from "../BlocklyInterface/Blockly";
 import { RobotSimulator } from "../RobotSimulator/RobotSimulator";
 import { ControlPanel } from "../ControlPanel/ControlPanel";
 import "./SimulatorView.css";
+import { JavascriptVM } from "../JavascriptVM/JavascriptVM";
 
 export const SimulatorView: FunctionComponent = () => {
   return (
     <div className="content">
       <Blockly />
+      <JavascriptVM />
       <RobotSimulator />
       <ControlPanel />
     </div>
