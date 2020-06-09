0569b236751d3b0e9b13e2d46284260ea5f08099
Gino Notto <ginojnotto@gmail.com>
2020-06-08 20:22:20 -0400

    setup working tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,37 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+// Sets up a virtual canvas for blockly
+import "jest-canvas-mock";
+
+// Sets up fluent ui icons
+import { initializeIcons } from "@uifabric/icons";
+initializeIcons();
+
+/**
+ * Simulator does not render and is outside of the scope of unit / integration tests.
+ */
+jest.mock("@fruk/simulator-core", () => {
+  return {
+    Sim3D: jest.fn(() => ({
+      beginRendering: jest.fn(),
+      stopRendering: jest.fn(),
+      onresize: jest.fn(),
+    })),
+  };
+});
+
+// Blockly issues warnings about not being able to
+const logFn = console.log.bind(console);
+console.log = (msg: any, ...rest: any[]) => {
+  if (
+    msg &&
+    typeof msg === "string" &&
+    msg.includes("WARNING: No message string for")
+  ) {
+    return;
+  }
+
+  logFn(msg, ...rest);
+};
