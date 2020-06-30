6773ca9878a1e23fbd9fe3dc88e403a29942a264
Matthias <eurocoinsalbum@users.noreply.github.com>
2020-06-30 14:24:05 +0200

    Game controller (#64)
    * added to gitignore
    
    * Added GameController
    
    * added dummy but correct motor controls
    
    * added license info for svg
    
    * added service function to control left and right motor
    
    * Added buttons from the steering cross
    
    * removed unwanted files

--- src/Views/SimulatorView.tsx
+++ src/Views/SimulatorView.tsx
@@ -3,6 +3,7 @@ import { RobotSimulator } from "../RobotSimulator/RobotSimulator";
 import { ControlPanel } from "../ControlPanel/ControlPanel";
 import "./SimulatorView.css";
 import { BlocklyEditor } from "../BlocklyInterface/BlocklyEditor";
+import { GameController } from "../Views/GameController/GameController";
 
 export const SimulatorView: FunctionComponent = () => {
   return (
@@ -10,6 +11,7 @@ export const SimulatorView: FunctionComponent = () => {
       <BlocklyEditor />
       <RobotSimulator />
       <ControlPanel />
+      <GameController />
     </div>
   );
 };
