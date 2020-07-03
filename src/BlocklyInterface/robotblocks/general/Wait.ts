7ccaa6579be546a72b05b1bd2991417310ca00d3
eurocoinsalbum <matthias@online.de>
2020-07-03 21:10:49 +0200

    added missing changes from last conflict

---
+++ src/BlocklyInterface/robotblocks/general/Wait.ts
@@ -0,0 +1,33 @@
+import { addCustomBlock, JavaScript } from "../AddBlockUtil";
+
+// code for wait block and VM-logic adopted from official blockly examples
+// on https://groups.google.com/forum/#!topic/blockly/5xbDQ8FQzbI
+export function addWaitBlock() {
+  addCustomBlock(
+    "wait",
+    [
+      {
+        type: "wait",
+        message0: "wait %1 seconds",
+        args0: [
+          {
+            type: "field_number",
+            name: "wait",
+            value: 0,
+            min: 0,
+          },
+        ],
+        inputsInline: false,
+        previousStatement: null,
+        nextStatement: null,
+        colour: 60,
+        tooltip: "",
+        helpUrl: "",
+      },
+    ],
+    (block) => {
+      const seconds = block.getFieldValue("wait");
+      return `wait(${seconds * 1000});\n`;
+    }
+  );
+}
