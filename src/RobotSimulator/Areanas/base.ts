3acfee326ddc01060c6dbba0d55ab83ef02f0dac
James Peach <james_peach01@hotmail.co.uk>
2020-08-24 15:43:49 +0100

    add challenge listener design

--- src/RobotSimulator/Areanas/base.ts
+++ src/RobotSimulator/Areanas/base.ts
@@ -13,4 +13,33 @@ export interface ChallengeConfig {
   startPosition: CoreSimTypes.Vector2d;
   finishZoneSpec?: CoreSpecs.IZoneSpec;
   arenaConfig: ArenaConfig;
+  eventListener?: ChallengeListener;
+}
+
+// interface to trigger actions and events in the current challenge.
+export interface ChallengeActions {
+  addObject(
+    o: CoreSpecs.IBallSpec | CoreSpecs.IBoxSpec | CoreSpecs.IConeSpec
+  ): void;
+}
+
+export interface ZoneEvent {
+  kind: "ZoneEvent";
+  zoneId: string;
+  entry: boolean;
+}
+
+export interface CollisionEvent {
+  kind: "CollisionEvent";
+  objectA: string;
+  objectB: string;
+}
+
+export type ChallengeEvent = ZoneEvent | CollisionEvent;
+
+export interface ChallengeListener {
+  onStart(actions: ChallengeActions): void;
+  onStop(): void;
+
+  onEvent(e: ChallengeEvent): void;
 }
