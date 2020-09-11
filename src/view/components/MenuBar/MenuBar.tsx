a06ca72df98fb6d5e36d68393761301110ee096b
Gino Notto <ginojnotto@gmail.com>
2020-09-11 13:21:55 -0400

    * menubar -> toolbar * menubar floats on bottom of screen

--- src/view/components/MenuBar/MenuBar.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const MenuBar = () => <div>MenuBar</div>;
---
+++ src/view/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../Common/Button";
+import "./Toolbar.css";
+
+export const Toolbar = () => (
+  <div className="toolbar">
+    <Button>A</Button>
+    <Button>A</Button>
+    <Button>A</Button>
+    <Button>A</Button>
+  </div>
+);
