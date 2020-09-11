89719932a0f402a87d540c77f40275457600e3be
Gino Notto <ginojnotto@gmail.com>
2020-09-11 19:34:45 -0400

    move toolbar to top under header

--- src/view/views/BlocklyView.tsx
+++ src/view/views/BlocklyView.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Container } from "../components/Common/Container";
 import { ButtonBar, Button, ButtonVariant } from "../components/Common/Button";
 import { BlocklyEditor } from "../../BlocklyInterface/BlocklyEditor";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { isExecuting, getExecutionState } from "../../JavascriptVM/vmSlice";
 import { ExecutionState } from "../../JavascriptVM/vm";
 import { IconName } from "../components/Common/Icon";
 import { useVM } from "../../JavascriptVM/JavascriptVM";
+import { getActiveEditor, editorSlice } from "../../Editor/editorSlice";
+import { SourceView } from "../../Editor/SourceView";
 
 const VMControls = () => {
   const isVMStarted = useSelector(isExecuting);
@@ -62,14 +64,49 @@ const VMControls = () => {
   );
 };
 
+const EditorControls = () => {
+  const dispatch = useDispatch();
+  const currentView = useSelector(getActiveEditor);
+  const changeViewCallback = useCallback(() => {
+    dispatch(
+      editorSlice.actions.setActiveEditor({
+        editor: currentView === "Blockly" ? "Source" : "Blockly",
+      })
+    );
+  }, [currentView, dispatch]);
+
+  return (
+    <ButtonBar>
+      <Button
+        variant={ButtonVariant.info}
+        iconName={IconName.save}
+        disabled={true}
+        title="Functionality coming soon"
+      >
+        Save Program
+      </Button>
+      <Button
+        variant={ButtonVariant.info}
+        iconName={IconName.view}
+        onClick={changeViewCallback}
+      >
+        View {currentView === "Blockly" ? "Source" : "Blocks"}
+      </Button>
+    </ButtonBar>
+  );
+};
+
 export const BlocklyView = () => {
+  const currentView = useSelector(getActiveEditor);
+
   return (
     <>
       <Container className="simulator-view--panel__main">
-        <BlocklyEditor />
+        {currentView === "Blockly" ? <BlocklyEditor /> : <SourceView />}
       </Container>
       <Container className="simulator-view--panel__utility">
         <VMControls />
+        <EditorControls />
       </Container>
     </>
   );
