8b59f8fbf953b7524b2f82adc7b8bb6d44b0cc80
Gino Notto <ginojnotto@gmail.com>
2020-09-11 13:24:06 -0400

    SimulatorView CSS

--- src/view/views/SimulatorView.tsx
+++ src/view/views/SimulatorView.tsx
@@ -1,9 +1,31 @@
 import React from "react";
-import { MenuBar } from "../../Views/MenuBar/MenuBar";
+import { Toolbar } from "../components/Toolbar/Toolbar";
+import { Container } from "../components/Common/Container";
+import { RobotSimulator } from "../../RobotSimulator/RobotSimulator";
+import "./SimulatorView.css";
+
+export const LeftPanel = () => (
+  <div className="simulator-view--panel">
+    <Container className="simulator-view--panel__main">
+      Left Panel (Main)
+    </Container>
+    <Container className="simulator-view--panel__utility">
+      Left Panel (Utility)
+    </Container>
+  </div>
+);
+export const RightPanel = () => (
+  <div className="simulator-view--panel">
+    <Container>
+      <RobotSimulator />
+    </Container>
+  </div>
+);
 
 export const SimulatorView = () => (
-  <div>
-    SimulatorView
-    <MenuBar />
+  <div className="simulator-view">
+    <LeftPanel />
+    <RightPanel />
+    <Toolbar />
   </div>
 );
