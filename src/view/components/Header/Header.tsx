7ed0bbbd557b48c79ddfef59b8d3152a5e3294ea
Gino Notto <ginojnotto@gmail.com>
2020-09-11 19:59:23 -0400

    display lesson / challenge name

--- src/view/components/Header/Header.tsx
+++ src/view/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { Divider } from "../Common/Divider";
-import { Link } from "react-router-dom";
+import { Link, useParams, useRouteMatch } from "react-router-dom";
 import brandImage from "./FIRST_HorzRGB_reverse.png";
 
 export const Brand = () => {
@@ -12,9 +12,22 @@ export const Brand = () => {
   );
 };
 
-export const Header = () => (
-  <div className="header">
-    <Brand />
-    <Divider vertical />
-  </div>
-);
+export const Header = () => {
+  const match = useRouteMatch<{ lesson: string; challenge: string }>({
+    path: "/lessons/:lesson/challenges/:challenge/",
+    strict: true,
+    sensitive: true,
+  });
+
+  return (
+    <div className="header">
+      <Brand />
+      <Divider vertical />
+      {match ? (
+        <h1>
+          {match.params.lesson} - {match.params.challenge}
+        </h1>
+      ) : null}
+    </div>
+  );
+};
