24830336962de65589710f3b5427592afaedbf30
Gino Notto <ginojnotto@gmail.com>
2020-09-13 12:01:52 -0400

    use As utility component

--- src/view/components/Common/Card.tsx
+++ src/view/components/Common/Card.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 import "./Card.css";
+import { As } from "./Utilities/As";
 
 export const Card: FunctionComponent = ({ children }) => (
   <div className="card">{children}</div>
@@ -15,16 +16,12 @@ export const CardTitle: FunctionComponent<{
   title: string;
   as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }> = ({ title, as }) => {
-  const renderMap: Record<typeof as, React.ReactElement> = {
-    h1: <h1 className="card--title">{title}</h1>,
-    h2: <h2 className="card--title">{title}</h2>,
-    h3: <h3 className="card--title">{title}</h3>,
-    h4: <h4 className="card--title">{title}</h4>,
-    h5: <h5 className="card--title">{title}</h5>,
-    h6: <h6 className="card--title">{title}</h6>,
-  };
-
-  return renderMap[as];
+  const className = "card--title";
+  return (
+    <As as={as} className={className}>
+      {title}
+    </As>
+  );
 };
 
 export const CardBody: FunctionComponent = ({ children }) => {
