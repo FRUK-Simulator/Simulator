bcfafeea3ac65efa5be0692e621b16ddc7399c8f
Gino Notto <ginojnotto@gmail.com>
2020-09-13 12:01:01 -0400

    As utility component

---
+++ src/view/components/Common/Utilities/As.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FunctionComponent } from "react";
+
+/**
+ * Utility component to render children under a specific html tag.
+ *
+ * @param props props
+ * @param props.className the className for the node
+ * @param props.as the element to render the children under
+ */
+export const As: FunctionComponent<{
+  className?: string;
+  as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+}> = ({ children, as, className = "" }) => {
+  const renderMap: Record<typeof as, () => React.ReactElement> = {
+    h1: () => <h1 className={className}>{children}</h1>,
+    h2: () => <h2 className={className}>{children}</h2>,
+    h3: () => <h3 className={className}>{children}</h3>,
+    h4: () => <h4 className={className}>{children}</h4>,
+    h5: () => <h5 className={className}>{children}</h5>,
+    h6: () => <h6 className={className}>{children}</h6>,
+  };
+
+  return renderMap[as]();
+};
