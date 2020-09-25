df06dd62a4df89a1f12dc840f2d90fb55de23fe1
Casper Oakley <coakley10@bloomberg.net>
2020-09-25 15:31:39 +0100

    Rename arenas path to fix spelling

--- src/RobotSimulator/Arenas/parkinglot.ts
+++ src/RobotSimulator/Arenas/parkinglot.ts
@@ -1,57 +1,47 @@
-import {
-  ArenaConfig,
-  ChallengeConfig,
-  ChallengeListener,
-  ChallengeActions,
-  ChallengeEvent,
-} from "./base";
-import { CoreSimTypes } from "@fruk/simulator-core";
-import { MessageType } from "../../state/messagesSlice";
+import { ArenaConfig, ChallengeConfig } from "./base";
 
 export const arenas = [arena];
 export const challenges = [challengeA, challengeB];
 
 function arena(): ArenaConfig {
-  const height: number = 0.4;
-  const thickness: number = 0.1;
+  const height: number = 1;
+  const thickness: number = 0.3;
+  const length: number = 4;
+  const width: number = 3;
   const arenaConfig: ArenaConfig = {
     name: "Parking Lot",
     worldConfig: {
-      zLength: 5,
-      xLength: 5,
+      zLength: 30,
+      xLength: 20,
       walls: [],
-      perimeter: {
-        height: 0.5,
-        thickness: 0.2,
-      },
       camera: {
         position: {
           x: 0,
-          y: 3,
-          z: 3,
+          y: 8,
+          z: 10,
         },
       },
     },
     boxSpecs: [
       {
         type: "box",
-        dimensions: { x: thickness, y: height, z: 1 },
-        initialPosition: { x: 2, y: -2 },
+        dimensions: { x: thickness, y: height, z: length },
+        initialPosition: { x: 9, y: -13 },
       },
       {
         type: "box",
-        dimensions: { x: thickness, y: height, z: 1 },
-        initialPosition: { x: 1, y: -2 },
+        dimensions: { x: thickness, y: height, z: length },
+        initialPosition: { x: 5, y: -13 },
       },
       {
         type: "box",
-        dimensions: { x: 1, y: height, z: thickness },
-        initialPosition: { x: -2, y: -2 },
+        dimensions: { x: width, y: height, z: thickness },
+        initialPosition: { x: -8.5, y: -12 },
       },
       {
         type: "box",
-        dimensions: { x: 1, y: height, z: thickness },
-        initialPosition: { x: -2, y: -1 },
+        dimensions: { x: width, y: height, z: thickness },
+        initialPosition: { x: -8.5, y: -8 },
       },
     ],
   };
@@ -62,9 +52,8 @@ function arena(): ArenaConfig {
 function challengeA(): ChallengeConfig {
   const challengeConfig: ChallengeConfig = {
     name: "Parking Lot Challenge 1",
-    startPosition: { x: -2, y: 2 },
+    startPosition: { x: 0, y: 0 },
     arenaConfig: arena(),
-    eventListener: new ParkingLotChallenge({ x: 1.5, y: -2 }),
   };
 
   return challengeConfig;
@@ -73,44 +62,9 @@ function challengeA(): ChallengeConfig {
 function challengeB(): ChallengeConfig {
   const challengeConfig: ChallengeConfig = {
     name: "Parking Lot Challenge 2",
-    startPosition: { x: 2, y: 2 },
+    startPosition: { x: 0, y: 0 },
     arenaConfig: arena(),
-    eventListener: new ParkingLotChallenge({ x: -2, y: -1.5 }),
   };
 
   return challengeConfig;
-}
-
-const FinishZoneId = "finish-zone";
-
-class ParkingLotChallenge implements ChallengeListener {
-  constructor(public finishPosition: CoreSimTypes.Vector2d) {}
-  actions?: ChallengeActions;
-
-  onStart(actions: ChallengeActions) {
-    this.actions = actions;
-    actions.addObject({
-      type: "zone",
-      initialPosition: this.finishPosition,
-      zoneId: FinishZoneId,
-      zoneShape: {
-        type: "rectangle",
-        zLength: 0.8,
-        xLength: 0.8,
-      },
-      baseColor: 0x00ff00,
-    });
-  }
-
-  onStop() {
-    this.actions = undefined;
-  }
-
-  onEvent(e: ChallengeEvent) {
-    if (e.kind === "ZoneEvent") {
-      if (e.zoneId === FinishZoneId) {
-        this.actions?.displayMessage("Robot Wins!", MessageType.success);
-      }
-    }
-  }
 }
