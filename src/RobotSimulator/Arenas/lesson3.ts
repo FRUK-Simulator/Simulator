0b65aaf979119b809115f3269f1daa5c7575636c
James Peach <james_peach01@hotmail.co.uk>
2021-09-07 15:27:08 +0100

    add mini arena for learning blocks

--- src/RobotSimulator/Arenas/lesson3.ts
+++ src/RobotSimulator/Arenas/lesson3.ts
@@ -9,15 +9,15 @@ import { IBoxSpec } from "@fruk/simulator-core/dist/engine/specs/CoreSpecs";
 import { ChallengeStatus } from "./challengeSlice";
 import { MessageType } from "../../state/messagesSlice";
 
-export const arenas = [arena];
-export const challenges = [challengeA, challengeB];
+export const arenas = [largeArena];
+export const challenges = [challengeA, challengeB, challengeC];
 
 enum Lesson3Goal {
   GET_TO_FINISH,
   MOVE_BLOCK_TO_START,
 }
 
-function arena(): ArenaConfig {
+function largeArena(): ArenaConfig {
   const arenaConfig: ArenaConfig = {
     name: "Lesson 3 - Advanced Driving",
     worldConfig: {
@@ -149,11 +149,66 @@ function arena(): ArenaConfig {
   return arenaConfig;
 }
 
+function smallArena(): ArenaConfig {
+  const arenaConfig: ArenaConfig = {
+    name: "Lesson 3 - Advanced Driving",
+    worldConfig: {
+      zLength: 5,
+      xLength: 1,
+      walls: [],
+      perimeter: {
+        height: 0.5,
+        thickness: 0.1,
+      },
+      camera: {
+        position: {
+          x: 3,
+          y: 3,
+          z: 3,
+        },
+      },
+    },
+    zoneSpecs: [
+      // Start / end zones
+      {
+        type: "zone",
+        zoneId: "start",
+        baseColor: 0x006600,
+        initialPosition: {
+          x: 0,
+          y: 2,
+        },
+        zoneShape: {
+          type: "rectangle",
+          zLength: 1,
+          xLength: 1,
+        },
+      },
+      {
+        type: "zone",
+        zoneId: "end",
+        baseColor: 0x660000,
+        initialPosition: {
+          x: 0,
+          y: -2,
+        },
+        zoneShape: {
+          type: "rectangle",
+          zLength: 1,
+          xLength: 1,
+        },
+      },
+    ],
+  };
+
+  return arenaConfig;
+}
+
 function challengeA(): ChallengeConfig {
   const challengeConfig: ChallengeConfig = {
     name: "Lesson 3 - Challenge A",
     startPosition: { x: -3.5, y: 2 },
-    arenaConfig: arena(),
+    arenaConfig: largeArena(),
     eventListener: new Lesson3Challenge(Lesson3Goal.GET_TO_FINISH),
     descriptions: {
       short: "Move along the path",
@@ -173,13 +228,57 @@ If the robot enters the black zones then you must start again.
 function challengeB(): ChallengeConfig {
   const challengeConfig: ChallengeConfig = {
     name: "Lesson 3 - Challenge B",
-    startPosition: { x: -3.5, y: 2 },
-    arenaConfig: arena(),
+    startPosition: { x: 0, y: 2 },
+    arenaConfig: smallArena(),
     eventListener: new Lesson3Challenge(Lesson3Goal.MOVE_BLOCK_TO_START),
     descriptions: {
       short: "Picking up blocks",
       markdown: `
-# Lesson 3 - Challenge A
+# Lesson 3 - Challenge B
+
+Drive the robot from the starting position along the path to the end.
+
+Pick up the block using the grabber. Then drive it back to the starting position.
+
+If the robot enters the black zones then you must start again.
+      `,
+    },
+  };
+
+  let box: IBoxSpec = {
+    type: "box",
+    id: "box",
+    dimensions: {
+      x: 0.25,
+      y: 0.25,
+      z: 0.25,
+    },
+    initialPosition: {
+      x: 0,
+      y: -2,
+    },
+    baseColor: 0xffffff,
+    isStatic: false,
+  };
+
+  if (!challengeConfig.arenaConfig.boxSpecs) {
+    challengeConfig.arenaConfig.boxSpecs = [];
+  }
+  challengeConfig.arenaConfig.boxSpecs.push(box);
+
+  return challengeConfig;
+}
+
+function challengeC(): ChallengeConfig {
+  const challengeConfig: ChallengeConfig = {
+    name: "Lesson 3 - Challenge C",
+    startPosition: { x: -3.5, y: 2 },
+    arenaConfig: largeArena(),
+    eventListener: new Lesson3Challenge(Lesson3Goal.MOVE_BLOCK_TO_START),
+    descriptions: {
+      short: "Picking up blocks",
+      markdown: `
+# Lesson 3 - Challenge C
 
 Drive the robot from the starting position along the path to the end.
 
